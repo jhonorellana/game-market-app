a595cc9b90237499aa0b0b1f41d255d6a5455499
Jhon Alfredo Orellana Ocaña <jhonorellana@gmail.com>
2024-01-26 18:54:41 -0500

    Ejemplo de manipulación de objetos con Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'game-market-app';
+  descripcion = 'Este es un sitio para venta de juegos'
+  MostrarDescipcion: boolean = true;
+  juegos: string[] = ['MArio Kart', 'AC1', 'Fifa 2023', 'minecraft'];
+
+  haciendoClick(): void{
+    alert('Has hecho click');
+  }
+
+  cuandoCambieAlgo(event: any): void{
+      console.log('Escribiste: ', event.target.value);
+  }
+
+  pasaMouse(): void{
+      console.log("Mouse sobre elemento");
+  }
+
+  saleMouse(){
+    console.log("Mouse sale del elemento");
+  }
+
+
 }
